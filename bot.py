f7b6f739575c746ec1e1ceb5d76349808669a584
crja73 <57744842+crja73@users.noreply.github.com>
2022-09-02 13:08:56 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -15,10 +15,11 @@ import keyboards as kb
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-
+o = []
 
 class Form(StatesGroup):
     admin = State()
+    order = State()
     
 
 @dp.callback_query_handler(text_startswith='btn')
@@ -30,6 +31,7 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
         code = int(code)
     if code == 0:
         await bot.send_message(callback_query.from_user.id, "Дайте точное описание вашему заказу, укажите желаемую стоимость проекта и срок, обязательно укажите свой тег telegram через '@', при необходимости свяжитесь с администратором", reply_markup=kb.greet_kb1)
+        await Form.order.set()
     if code == 4:
         await bot.send_message(callback_query.from_user.id, "Связаться напрямую -> @rolex0nmywrist")
         
@@ -37,31 +39,16 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
         await bot.send_message(callback_query.from_user.id, "https://lolz.guru/threads/3933835/")
 
 
-        
 
-    # if code == 2:
-    #     await bot.answer_callback_query(callback_query.id, text='Нажата вторая кнопка')
-    # elif code == 5:
-    #     await bot.answer_callback_query(
-    #         callback_query.id,
-    #         text='Нажата кнопка с номером 5.\nА этот текст может быть длиной до 200 символов 😉',
-    #         show_alert=True)
-    # else:
-    #     await bot.answer_callback_query(callback_query.id)
-    # await bot.send_message(callback_query.from_user.id, code)
-
-
-##
-
-
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'menu'])
 async def process_start_command(message: types.Message):
     await bot.send_message(message.from_user.id, "Меню", reply_markup=kb.inline_kb_full)
 
-@dp.message_handler(commands=['menu'])
-async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, "Меню", reply_markup=kb.inline_kb_full)
 
+@dp.message_handler(commands=['admin'])
+async def adminka(message: types.Message):
+    await message.answer('Обезьяна')
+    await Form.admin.set()
 
 
 @dp.message_handler(Text(equals="Обратно в меню"))
@@ -71,19 +58,7 @@ async def menu(message: types.Message):
     await message.answer("Меню", reply_markup=kb.inline_kb_full)
 
 
-@dp.message_handler(Text(contains="@"))
-async def order(message: types.Message):
-    await message.answer('Ваш заказ принят. Ожидайте, в ближайшее время с вами свяжутся')
 
-
-@dp.message_handler(commands=['admin'])
-async def adminka(message: types.Message):
-
-    
-
-    await message.answer('Обезьяна')
-
-    await Form.admin.set()
 
 
 @dp.message_handler(state=Form.admin)
@@ -93,8 +68,24 @@ async def process_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         password = message.text
     if password == 'admin':
+        await message.answer(o)
         await Form.next()
-        await message.answer('Чувашия')
+
+@dp.message_handler(state=Form.order)
+async def ordering(message: types.Message, state: FSMContext):
+    global o
+    async with state.proxy() as data:
+        ordr = message.text
+        
+    if len(list(ordr)) > 20:
+
+        ind = len(o) + 1
+        o.append([ind, ordr])
+        await message.answer('Ваш заказ принят. Ожидайте, в ближайшее время с вами свяжутся')
+        await bot.send_message('1017470547', '{} - {}'.format(o[-1][0], o[-1][1]))
+    await Form.next()
+        
+
 
 
 # @dp.callback_query_handler(text="back_menu")
